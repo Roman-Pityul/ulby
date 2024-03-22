e48c0865a30d13a06271c2f97a717d14b374bdb7
Roman Pityul <r.pityul@heliburo.com>
2024-03-22 16:42:23 +0300

    22.03.2024

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -2,7 +2,7 @@ import webpack from 'webpack'
 import MiniCssExtractPlugin from 'mini-css-extract-plugin'
 import { BuildOptions } from './types/config'
 
-export function buildLoaders (options: BuildOptions): webpack.RuleSetRule[] {
+export function buildLoaders ({isDev}: BuildOptions): webpack.RuleSetRule[] {
 
   const typescriptLoader =  {
     test: /\.tsx?$/,
@@ -13,11 +13,14 @@ export function buildLoaders (options: BuildOptions): webpack.RuleSetRule[] {
   const cssLoader = {
       test: /\.s[ac]ss$/i,
       use: [
-        options.isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+        isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
         {
           loader: "css-loader",
           options: {
-            modules: true
+            modules: {
+              auto: (resPath: string) => Boolean(resPath.includes('.module.')),
+              localIdentName: isDev ? '[path][name]__[local]' : '[hash:base64:8]'
+            }, 
           }
         },
         "sass-loader",
